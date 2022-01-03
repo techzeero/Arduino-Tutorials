8bc750e1bdf7dae400bafe71100c12a1521954cd
Techzeero <56719352+techzeero@users.noreply.github.com>
2022-01-03 13:12:26 +0530

    Create Multiple LEDs With an Arduino Code2.ino

---
+++ Multiple LEDs With an Arduino Code2.ino
@@ -0,0 +1,37 @@
+/* 
+   Controlling Multiple LEDs With an Arduino
+   For more details, visit: https://techzeero.com/arduino-tutorials/multiple-leds-blinking-using-arduino/
+*/
+
+void setup()
+{
+  pinMode(7, OUTPUT);
+  pinMode(6, OUTPUT);
+  pinMode(5, OUTPUT);
+  pinMode(4, OUTPUT);
+  pinMode(3, OUTPUT);
+}
+
+void loop()
+{
+  digitalWrite(7, HIGH);
+  digitalWrite(6, HIGH);
+  digitalWrite(5, HIGH);
+  digitalWrite(4, HIGH);
+  digitalWrite(3, HIGH);
+  delay(200); // Wait for 200 millisecond(s)
+  digitalWrite(7, LOW);
+  delay(200); // Wait for 200 millisecond(s)
+  digitalWrite(6, LOW);
+  digitalWrite(7, HIGH);
+  delay(200); // Wait for 200 millisecond(s)
+  digitalWrite(5, LOW);
+  digitalWrite(6, HIGH);
+  delay(200); // Wait for 200 millisecond(s)
+  digitalWrite(4, LOW);
+  digitalWrite(5, HIGH);
+  delay(200); // Wait for 200 millisecond(s)
+  digitalWrite(3, LOW);
+  digitalWrite(4, HIGH);
+  delay(200); // Wait for 200 millisecond(s)
+}
